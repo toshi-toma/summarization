dd90bc628b5fdb735bbf1e2cdd2fa3c319938441
toshi1217 <b1595204@planet.kanazawa-it.ac.jp>
2016-10-08 02:49:07 +0900

    ニュース本文のHTML取得 fix #2

--- scraping_news.py
+++ scraping_news.py
@@ -2,14 +2,30 @@
 import requests
 from bs4 import BeautifulSoup
 
+#livedoorニュースのカテゴリ「主要」ページのHTML取得
 response = requests.get('http://news.livedoor.com/topics/category/main/')
-# print response.status_code
-# print response.headers
-# print response.encoding
-# print response.text
+# print response.status_code + response.headers + response.encoding + response.text
 soup = BeautifulSoup(response.text,"lxml")
+
+#HTML内のニュース一覧部分を取得
 mainbody = soup.find(class_='mainBody')
-url_list = []
+
+#主要ニュースTOP20のURLリスト
+urllist = []
+
+#ニュースのURL取得及び格納
 for link in mainbody.findAll("a"):
-    url_list.append(link.get('href'))
-print url_list
+    #url内の文字列topicsをarticleに変更=>ニュース本文を取得
+    article_link = link.get('href').replace('topics', 'article')
+    urllist.append(article_link)
+
+print urllist
+
+#ニュース本文HTMLを取得
+for link in urllist:
+    news_html = requests.get(link)
+    news_soup = BeautifulSoup(news_html.text,"lxml")
+    
+
+
+
