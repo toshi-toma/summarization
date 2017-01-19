7eab88c57ae2c3992b947ca4ca2e8729ccbb9ad2
tosshyt <b1595204@planet.kanazawa-it.ac.jp>
2017-01-19 14:12:09 +0900

    logistic_regressionにグリッドサーチ実装

--- model/logistic_regression.py
+++ model/logistic_regression.py
@@ -4,6 +4,8 @@ from sklearn import linear_model
 from sklearn.cross_validation import train_test_split
 import pandas as pd
 from sklearn.metrics import classification_report
+from sklearn.linear_model import LogisticRegression
+from sklearn.grid_search import GridSearchCV
 
 #訓練データ用CSVファイル
 TRAIN_FILE = '../data/train_data.csv'
@@ -13,11 +15,14 @@ def logistic_regression():
     x = data[:, 0:10]
     y = data[:, 10]
     X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
-    log_reg = linear_model.LogisticRegression()
-    log_reg.fit(X_train, y_train)
-    py = log_reg.predict(X_test)
+    params = {'C': [0.001, 0.01, 0.1, 1, 10, 100]}
+
+    grid = GridSearchCV(LogisticRegression(), param_grid=params)
+    grid.fit(X_train, y_train)
+    py = grid.predict(X_test)
     table = pd.crosstab(y_test, py)
     print table
+    print grid.best_params_
     print(classification_report(y_test, py))
 if __name__ == '__main__':
     logistic_regression()
