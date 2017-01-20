cd490f0de8adf98159953487bd36aaf0ce96ef91
tosshyt <b1595204@planet.kanazawa-it.ac.jp>
2017-01-20 14:30:03 +0900

    グリッドサーチによるパラメータ最適化 fix #54

--- model/logistic_regression.py
+++ model/logistic_regression.py
@@ -19,13 +19,32 @@ def logistic_regression():
     y = data[:, 15]
     X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
     params = {'C': [0.001, 0.01, 0.1, 1, 10, 100]}
+    score = 'f1'
+    clf = GridSearchCV(
+        LogisticRegression(),  # 識別器
+        params,  # 最適化したいパラメータセット
+        cv=5,  # 交差検定の回数
+        scoring='%s_weighted' % score)  # モデルの評価関数の指定
+    clf.fit(X_train, y_train)
+    print clf.grid_scores_
+    print clf.best_params_
+    print("# Tuning hyper-parameters for %s" % score)
+    print()
+    print("Best parameters set found on development set: %s" % clf.best_params_)
+    print()
 
-    grid = GridSearchCV(LogisticRegression(), param_grid=params)
-    grid.fit(X_train, y_train)
-    py = grid.predict(X_test)
-    table = pd.crosstab(y_test, py)
-    print table
-    print grid.best_params_
-    print(classification_report(y_test, py))
+    # それぞれのパラメータでの試行結果の表示
+    print("Grid scores on development set:")
+    print()
+    for params, mean_score, scores in clf.grid_scores_:
+        print("%0.3f (+/-%0.03f) for %r"
+              % (mean_score, scores.std() * 2, params))
+    print()
+
+    # テストデータセットでの分類精度を表示
+    print("The scores are computed on the full evaluation set.")
+    print()
+    y_true, y_pred = y_test, clf.predict(X_test)
+    print(classification_report(y_true, y_pred))
 if __name__ == '__main__':
     logistic_regression()
--- model/svm.py
+++ model/svm.py
@@ -5,6 +5,7 @@ import pandas as pd
 from sklearn.metrics import accuracy_score
 from sklearn.svm import SVC
 from sklearn.metrics import classification_report
+from sklearn.grid_search import GridSearchCV
 #訓練データ用CSVファイル
 TRAIN_FILE = '../data/train_data.csv'
 #訓練データ用CSVファイル2
@@ -15,11 +16,39 @@ def svm():
     x = data[:, 0:15]
     y = data[:, 15]
     X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
-    clf = SVC(C=100., gamma=0.1)
+    params = [
+        {'C': [1, 10, 100, 1000], 'kernel': ['linear']},
+        {'C': [1, 10, 100, 1000], 'kernel': ['rbf'], 'gamma': [0.001, 0.0001]},
+        {'C': [1, 10, 100, 1000], 'kernel': ['poly'], 'degree': [2, 3, 4], 'gamma': [0.001, 0.0001]},
+        {'C': [1, 10, 100, 1000], 'kernel': ['sigmoid'], 'gamma': [0.001, 0.0001]}
+    ]
+    score = 'f1'
+    clf = GridSearchCV(
+        SVC(),  # 識別器
+        params,  # 最適化したいパラメータセット
+        cv=5,  # 交差検定の回数
+        scoring='%s_weighted' % score)  # モデルの評価関数の指定
     clf.fit(X_train, y_train)
-    predict_result = clf.predict(X_test)
-    print pd.crosstab(y_test, predict_result)
-    print(classification_report(y_test, predict_result))
+    print clf.grid_scores_
+    print clf.best_params_
+    print("# Tuning hyper-parameters for %s" % score)
+    print()
+    print("Best parameters set found on development set: %s" % clf.best_params_)
+    print()
+
+    # それぞれのパラメータでの試行結果の表示
+    print("Grid scores on development set:")
+    print()
+    for params, mean_score, scores in clf.grid_scores_:
+        print("%0.3f (+/-%0.03f) for %r"
+              % (mean_score, scores.std() * 2, params))
+    print()
+
+    # テストデータセットでの分類精度を表示
+    print("The scores are computed on the full evaluation set.")
+    print()
+    y_true, y_pred = y_test, clf.predict(X_test)
+    print(classification_report(y_true, y_pred))
 
 
 if __name__ == '__main__':
